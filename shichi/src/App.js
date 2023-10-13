b9759eda62c46247cdd60b575f06b47fa4cbad01
morgan09mj <m.s.mousazade09@gmail.com>
2023-10-13 14:02:48 +0330

    install packages

---
+++ shichi/src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+
+
+
+
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
