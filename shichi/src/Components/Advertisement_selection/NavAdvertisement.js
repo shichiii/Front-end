c9f5d08076c77dd7877482f138775a24db874b0b
morgan09mj <m.s.mousazade09@gmail.com>
2023-10-20 17:41:09 +0330

    Advertisement selection
    feat : create new page for Advertisement car

---
+++ shichi/src/Components/Advertisement_selection/NavAdvertisement.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../HomePage/NavBar";
+
+const NavAdvertisement = () => {
+  const [isAdvertisementClicked, setIsAdvertisementClicked] = useState(true);
+  const [isRentalClicked, setIsRentalClicked] = useState(false);
+
+  const handleAdvertisementClick = () => {
+    setIsAdvertisementClicked(true);
+    setIsRentalClicked(false);
+  };
+
+  const handleRentalClick = () => {
+    setIsAdvertisementClicked(false);
+    setIsRentalClicked(true);
+  };
+
+  useEffect(() => {
+    
+  }, []); 
+
+  return (
+    <div>
+      <Navbar  />
+      <div className="bg-gradient-to-t from-pallate-Gunmetal via-pallate-Police_Blue to-pallate-Gunmetal h-24 flex w-full">
+        <div
+          onClick={handleAdvertisementClick}
+        >
+          Advertisement History
+        </div>
+        <div
+          onClick={handleRentalClick}
+          
+        >
+          Rental History
+        </div>
+      </div>
+    
+    </div>
+  );
+};
+
+export default NavAdvertisement;
