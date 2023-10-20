50af02c7f4878ab9b044c31fda34e412d04d7227
Hazhir2002 <Hazhiryousefi2002@gmail.com>
2023-10-20 11:27:22 +0330

    removed the two location and postal code input

--- shichi/src/Components/for_push/Profile/Input.js
+++ shichi/src/Components/for_push/Profile/Input.js
@@ -1,13 +1,30 @@
-function Input({ type, name, label, placeholder }) {
+import { useState } from "react";
+
+function Input({
+  type,
+  name,
+  label,
+  placeholder,
+  value: defaultValue,
+  onChange,
+  disabled,
+}) {
+  const [value, setValue] = useState(defaultValue);
+
   return (
     <div className="flex flex-col w-[300px]">
-      <label htmlFor={name} className="mb-2">
+      <label htmlFor={name} className="mb-2 text-sm">
         {label}
       </label>
       <input
+        disabled={disabled}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         type={type}
         name={name}
-        className=" flex p-1 w-full rounded-md text-black outline-none px-3 text-center opacity-90 focus:opacity-100"
+        className={`flex p-1 w-full rounded-md ${
+          disabled ? "text-slate-100" : "text-black"
+        } outline-none px-3 text-center opacity-90 focus:opacity-100`}
         placeholder={placeholder}
       />
     </div>
--- shichi/src/Components/for_push/Profile/ProfileImage.js
+++ shichi/src/Components/for_push/Profile/ProfileImage.js
@@ -1,17 +1,23 @@
+import { useRef } from "react";
 import logo from "../../../Static/whitelogo.svg";
 
 function ProfileImage() {
+  const fileRef = useRef();
+
   return (
-    <div className="flex flex-row justify-between items-center w-72 gap-2">
+    <div className="flex flex-col justify-between items-center gap-2">
       <img
         // onClick={() => setOpen(!open)}
         src="https://tecdn.b-cdn.net/img/new/avatars/1.webp"
         className="bg-pallate-Gunmetal w-40 cursor-pointer p-2 rounded-full shadow-lg"
         alt="Avatar"
       />
-      <div className="flex flex-col text-slate-300">
-        <span className="font-bold">Hazhir Yousefi</span>
-        <span>Tehran, Iran</span>
+      <input type="file" className="hidden" ref={fileRef} />
+      <div
+        className="flex flex-col text-slate-300 hover:cursor-pointer hover:text-blue-300"
+        onClick={() => fileRef.current.click()}
+      >
+        Set a new profile photo
       </div>
     </div>
   );
