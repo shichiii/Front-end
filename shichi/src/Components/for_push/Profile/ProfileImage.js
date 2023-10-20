37de4bf34279690a621fc1db5d8f77f10ce64450
Hazhir2002 <Hazhiryousefi2002@gmail.com>
2023-10-20 13:37:23 +0330

    added the image preview functionality

--- shichi/src/Components/for_push/Profile/ProfileImage.js
+++ shichi/src/Components/for_push/Profile/ProfileImage.js
@@ -1,18 +1,38 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import logo from "../../../Static/whitelogo.svg";
 
 function ProfileImage() {
   const fileRef = useRef();
+  const [imageUrl, setImageUrl] = useState(
+    "https://tecdn.b-cdn.net/img/new/avatars/1.webp"
+  );
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(files[i]);
+    }
+  };
 
   return (
     <div className="flex flex-col justify-between items-center gap-2">
       <img
         // onClick={() => setOpen(!open)}
-        src="https://tecdn.b-cdn.net/img/new/avatars/1.webp"
+        src={imageUrl}
         className="bg-pallate-Gunmetal w-40 cursor-pointer p-2 rounded-full shadow-lg"
         alt="Avatar"
       />
-      <input type="file" className="hidden" ref={fileRef} />
+      <input
+        type="file"
+        className="hidden"
+        ref={fileRef}
+        onChange={(e) => handleFileChange(e)}
+      />
       <div
         className="flex flex-col text-slate-300 hover:cursor-pointer hover:text-blue-300"
         onClick={() => fileRef.current.click()}
