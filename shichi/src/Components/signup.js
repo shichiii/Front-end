4c0539226105da46fe9916ff366b97266ec66c2d
anitatalkhabi <74290042+anitatalkhabi@users.noreply.github.com>
2023-11-02 16:28:33 +0330

    fix

--- shichi/src/Components/signup.js
+++ shichi/src/Components/signup.js
@@ -6,9 +6,31 @@ import { useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 function  Signup(){
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
   const [emailAddress , setEmailAddress] = useState("");
   const [password , setpassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("enter")
+    try{
+      const response = await axios.post("http://185.157.245.99:8000/user/signup/", {
+        email : emailAddress,
+        password : password,
+        first_name : firstname,
+        last_name : lastname,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      navigate("/home");
+      console.log(response.data);
+    }catch(error){
+      console.error(error);
+    }
+  }
 
   const handleEmail = (event) => {
     setEmailAddress(event.target.value);
@@ -16,12 +38,18 @@ function  Signup(){
   const handlePassword = (event) => {
     setpassword(event.target.value);
   }
+  const handleFirstname = (event) => {
+    setfirstname(event.target.value);
+  }
+  const handleLastname = (event) => {
+    setlastname(event.target.value);
+  }
 
   let navigate = useNavigate();
 
 
     return(
-        <div class="min-h-screen bg-gradient-to-r from-pallate-Dark_Sky_Blue from-10%  to-pallate-Police_Blue to-90%  py-6 flex flex-col justify-center sm:py-12">
+        <div class="min-h-screen bg-gradient-to-t from-pallate-Gunmetal via-pallate-Police_Blue to-pallate-Gunmetal py-6 flex flex-col justify-center sm:py-12">
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
     <div
       class="absolute inset-0 bg-pallate-Dark_Sky_Blue shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
@@ -35,19 +63,19 @@ function  Signup(){
         <div class="divide-y divide-gray-200">
           <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
           <div class="relative">
-              <input autocomplete="off" id="firstname" name="firstname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="First Name" onChange={handleEmail}/>
+              <input autoComplete="off" id="firstname" name="firstname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="First Name" onChange={handleFirstname}/>
               <label for="firstname" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm ">First Name</label>
             </div>
             <div class="relative">
-              <input autocomplete="off" id="lastname" name="lastname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Last Name" onChange={handleEmail}/>
+              <input autoComplete="off" id="lastname" name="lastname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Last Name" onChange={handleLastname}/>
               <label for="name" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm ">Last Name</label>
             </div>
             <div class="relative">
-              <input autocomplete="off" id="email" name="email" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleEmail}/>
+              <input autoComplete="off" id="email" name="email" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleEmail}/>
               <label for="email" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-6 peer-focus:text-gray-600 peer-focus:text-sm ">Email Address</label>
             </div>
             <div class="relative">
-              <input autocomplete="off" id="password" name="password" type="password" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"   onChange={handlePassword}/>
+              <input autoComplete="off" id="password" name="password" type="password" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"   onChange={handlePassword}/>
               <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
             </div>
             <div>
@@ -57,7 +85,7 @@ function  Signup(){
             </div>
             <div class="relative">
               <button class="bg-pallate-Dark_Sky_Blue font-medium w-full text-white rounded-md px-2 py-1" 
-              onClick={()=> navigate("/home")}
+              onClick={(handleSubmit)}
               >Submit</button>
             </div>
           </div>
