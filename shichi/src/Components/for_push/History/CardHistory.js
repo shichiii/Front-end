12dcce4214a3bf396544475150807a43a8e2a975
M.Sadegh MousaZade <m.s.mousazade09@gmail.com>
2023-11-02 20:52:31 +0330

    fix some problem
    fix : fix some problem about imports

--- shichi/src/Components/for_push/History/CardHistory.js
+++ shichi/src/Components/for_push/History/CardHistory.js
@@ -4,7 +4,9 @@ import Car from "../../../Static/110.png";
 const CardHistory = () => {
   return (
     <div className="flex flex-col ">
-      <div className="w-full flex h-80">
+
+
+      {/* <div className="w-full flex h-80">
         <div className="text-black justify-center items-center w-1/3 flex bg-white cursor-pointer mt-10">
           <img src={Car} alt="Your Image" class="object-cover " />
         </div>
@@ -26,68 +28,26 @@ const CardHistory = () => {
         <div className="text-black justify-center items-center w-1/3 flex bg-white cursor-pointer mt-10">
           <img src={Car} alt="Your Image" class="object-cover " />
         </div>
-      </div>
-      <div className=" h-auto mt-2 grid grid-cols-5 gap-3 mx-2 ">
-        <div className=" h-96 rounded-lg bg-orange-100 ">
+      </div> */}
+
+
+      <div className=" h-auto mt-4 grid grid-cols-5 gap-3 mx-10 ">
+
+        <div  className="Adcard h-full rounded-lg bg-orange-100/50 shadow-lg shadow-orange-400/80">
           <div className="grid w-full h-full justify-center content-between f-full gap-3 grid-cols-1 ">
             <img src={Car} alt="Card Car" class="object-cover"/>
             <div class="text-center">
-              <h1 class="text-4xl font-bold">Name</h1>
+              <h1 class="text-4xl font-bold">Car Name</h1>
               <p class="text-2xl text-stone-950">Model</p>
             </div>
-            <div class="text-right">
-              <p class="text-stone-950 text-2xl mx-3 my-3">yyyy/yy/y</p>
+            <div className="grid grid-cols-2">
+              <p class="text-stone-950 text-2xl mx-3 my-3 text-left">from : Example</p>
+              <p class="text-stone-950 text-2xl mx-3 my-3 text-right">Date : yyyy/yy/y</p>
             </div>
           </div>
         </div>
-        <div className=" h-96 rounded-lg bg-orange-100 ">
-          <div className="grid w-full h-full justify-center content-between f-full gap-3 grid-cols-1 ">
-            <img src={Car} alt="Card Car" class="object-cover"/>
-            <div class="text-center">
-              <h1 class="text-4xl font-bold">Name</h1>
-              <p class="text-2xl text-stone-950">Model</p>
-            </div>
-            <div class="text-right">
-              <p class="text-stone-950 text-2xl mx-3 my-3">yyyy/yy/y</p>
-            </div>
-          </div>
-        </div>
-        <div className=" h-96 rounded-lg bg-orange-100 ">
-          <div className="grid w-full h-full justify-center content-between f-full gap-3 grid-cols-1 ">
-            <img src={Car} alt="Card Car" class="object-cover"/>
-            <div class="text-center">
-              <h1 class="text-4xl font-bold">Name</h1>
-              <p class="text-2xl text-stone-950">Model</p>
-            </div>
-            <div class="text-right">
-              <p class="text-stone-950 text-2xl mx-3 my-3">yyyy/yy/y</p>
-            </div>
-          </div>
-        </div>
-        <div className=" h-96 rounded-lg bg-orange-100 ">
-          <div className="grid w-full h-full justify-center content-between f-full gap-3 grid-cols-1 ">
-            <img src={Car} alt="Card Car" class="object-cover"/>
-            <div class="text-center">
-              <h1 class="text-4xl font-bold">Name</h1>
-              <p class="text-2xl text-stone-950">Model</p>
-            </div>
-            <div class="text-right">
-              <p class="text-stone-950 text-2xl mx-3 my-3">yyyy/yy/y</p>
-            </div>
-          </div>
-        </div>
-        <div className=" h-96 rounded-lg bg-orange-100 ">
-          <div className="grid w-full h-full justify-center content-between f-full gap-3 grid-cols-1 ">
-            <img src={Car} alt="Card Car" class="object-cover"/>
-            <div class="text-center">
-              <h1 class="text-4xl font-bold">Name</h1>
-              <p class="text-2xl text-stone-950">Model</p>
-            </div>
-            <div class="text-right">
-              <p class="text-stone-950 text-2xl mx-3 my-3">yyyy/yy/y</p>
-            </div>
-          </div>
-        </div>
+
+
       </div>
     </div>
   );
