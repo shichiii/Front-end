1f73e9d2afda8cf70aaf0cda43232f6450123f29
fatemehnikpayan <fatemeh_nikpayan@comp.iust.ac.ir>
2023-11-03 20:28:50 +0330

    fix: redesign and check req

--- shichi/src/Components/signup.js
+++ shichi/src/Components/signup.js
@@ -61,21 +61,21 @@ function  Signup(){
         </div>
         <div class="divide-y divide-gray-200">
           <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-          <div class="relative">
-              <input autoComplete="off" id="firstname" name="firstname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="First Name" onChange={handleFirstname}/>
-              <label for="firstname" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm ">First Name</label>
+        <div class="relative pb-3">
+              <input autoComplete="off" id="firstname" name="firstname" type="text" class="rounded-md peer placeholder-transparent h-9 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="First Name" onChange={handleFirstname}/>
+              <label for="firstname" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5  ml-2  peer-focus:text-sm peer-focus:-top-6 peer-focus:text-white">First Name</label>
+            </div>
+            <div class="relative pb-3">
+              <input autoComplete="off" id="lastname" name="lastname" type="text" class="rounded-md peer placeholder-transparent h-9 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Last Name" onChange={handleLastname}/>
+              <label for="name" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:-top-6 peer-focus:text-white  ml-2 peer-focus:text-sm ">Last Name</label>
+            </div>
+            <div class="relative pb-3">
+              <input autoComplete="off" id="email" name="email" type="text" class="rounded-md peer placeholder-transparent h-9 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleEmail}/>
+              <label for="email" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-6 peer-focus:text-white ml-2 peer-focus:text-sm ">Email Address</label>
             </div>
             <div class="relative">
-              <input autoComplete="off" id="lastname" name="lastname" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Last Name" onChange={handleLastname}/>
-              <label for="name" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm ">Last Name</label>
-            </div>
-            <div class="relative">
-              <input autoComplete="off" id="email" name="email" type="text" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleEmail}/>
-              <label for="email" class=" absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-6 peer-focus:text-gray-600 peer-focus:text-sm ">Email Address</label>
-            </div>
-            <div class="relative">
-              <input autoComplete="off" id="password" name="password" type="password" class="rounded-md peer placeholder-transparent h-8 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"   onChange={handlePassword}/>
-              <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+              <input autoComplete="off" id="password" name="password" type="password" class="rounded-md peer placeholder-transparent h-9 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"   onChange={handlePassword}/>
+              <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4.5  peer-focus:-top-6 peer-focus:text-white ml-2 peer-focus:text-sm">Password</label>
             </div>
             <div>
             <Link to="/Login" className="hover:bg-AteneoBlue-400 focus:bg-AteneoBlue-500 inline-block text-sm text-white align-baseline py-2.5 w-full rounded bg-AteneoBlue-500 text-center focus:text-white">
