a8d96aefa7505444e8d272bbb37d58e1e9a6b2a2
anitatalkhabi <74290042+anitatalkhabi@users.noreply.github.com>
2023-11-03 19:47:17 +0330

    feat: some functionality

--- shichi/src/Components/addcar/popup.js
+++ shichi/src/Components/addcar/popup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 import { useState } from "react";
 import Modal from "react-modal";
 import DatePicker from 'react-datepicker';
@@ -12,20 +13,64 @@ const MyComponent = () => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [imgFile, setImgfile] = useState(null);
-  const [carname, setCarname] = usestate(null);
-  const [carcolor, setCarcolor] = usestate(null);
-  const [title, setTitle] = usestate(null);
+  const [carname, setCarname] = useState(null);
+  const [carcolor, setCarcolor] = useState(null);
+  const [title, setTitle] = useState(null);
   const [price,setPrice] = useState(null);
-  const [description, setDescription] = usestate(null);
+  const [description, setDescription] = useState(null);
   const [carmodel, setCarmodel] = useState(null);
   const [category, setCategory] = useState(null);
-
+  const handleCarname = (event) => {
+    setCarname(event.target.value);
+  };
+  const handleCarcolor = (event) => {
+    setCarcolor(event.target.value);
+  };
+  const handletitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleprice = (event) => {
+    setPrice(event.target.value);
+  };
+  const handledescription = (event) => {
+    setDescription(event.target.value);
+  };
+  const handleCarmodel = (event) => {
+    setCarmodel(event.target.value);
+  };
+  const handleCategory = (event) => {
+    setCategory(event.target.value);
+  };
+  const handleImage = (event) => {
+    setImgfile(event.target.value);
+  };
   const openModal = () => {
     setIsModalOpen(true);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
+    const data = {
+      startDate,
+      endDate,
+      imgFile,
+      carname,
+      carcolor,
+      title,
+      price,
+      description,
+      carmodel,
+      category
+  };
+  axios.post('185.157.245.99:8000/advertisement/create/', data)
+    .then(response => {
+      console.log("hi")
+      // Handle the server response if needed
+    })
+    .catch(error => {
+      // Handle errors
+    });
+
+  };
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -68,7 +113,7 @@ const MyComponent = () => {
     </label>
     <p>Please select your car image!</p>
     <div class="relative">
-      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 sr-only" id="grid-image" type="file"/>
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 sr-only" id="grid-image" type="file" onChange={handleImage}/>
       <label for="grid-image" class="absolute inset-0 flex items-center justify-center w-full h-full cursor-pointer">
         <i class="fas fa-upload" ></i> 
       </label>
@@ -81,19 +126,19 @@ const MyComponent = () => {
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
         Car Name 
       </label>
-      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text"/>
+      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" onChange={handleCarname}/>
     </div>
     <div class="md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
         Car Color 
       </label>
-      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text"/>
+      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" onChange={handleCarcolor}/>
     </div>
     <div class="md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
         Title 
       </label>
-      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text"/>
+      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" onChange={handletitle}/>
     </div>
   </div>
   <div class="-mx-3 md:flex mb-6">
@@ -121,25 +166,25 @@ const MyComponent = () => {
 </div>
   <div class="md:w-1/2 px-3">
     <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="end-date">Price</label>
-    <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3" id="end-date" type="number" min="0"/>
+    <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3" id="end-date" type="number" min="0" onChange={handleprice}/>
   </div>
 </div>
   <div class="-mx-3 md:flex mb-6">
   <div class="md:w-1/2 px-3">
     <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="end-date">Description</label>
-    <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-5 mb-4" id="end-date" type="text"/>
+    <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-5 mb-4" id="end-date" type="text" onChange={handledescription}/>
   </div>
   <div class="md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
         Car Model
       </label>
-      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text"/>
+      <input class="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" onChange={handleCarmodel}/>
     </div>
     <div className="md:w-1/2 px-3">
   <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" htmlFor="category">
     Category
   </label>
-  <select className="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="category">
+  <select className="appearance-none block w-full bg-pallate-Police_Blue opacity-50 text-grey-darker border border-grey-lighter rounded py-3 px-4" id="category" onChange={handleCategory}>
     <option value="">Select a category</option>
     <option value="option1">economy</option>
     <option value="option2"> luxuxry </option>
@@ -168,5 +213,4 @@ const MyComponent = () => {
     </div>
   );
 };
-
 export default MyComponent;
