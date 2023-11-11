b851f17570a19af37ebe350721a03d47956cd934
fatemehnikpayan <fatemeh_nikpayan@comp.iust.ac.ir>
2023-11-12 01:52:48 +0330

    refactor: AddCar design refactor

---
+++ shichi/src/Components/addcar/CarApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Advertise from './Advertise'
+import Navbar from '../for_push/HomePage/NavBar'
+import Footer from '../for_push/HomePage/Footer'
+const CarApp = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Advertise/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default CarApp
---
+++ shichi/src/Components/addcar/Data.js
@@ -0,0 +1,330 @@
+import economic from "../../Static/110.png";
+import minivan from "../../Static/110.png";
+import affroad from "../../Static/110.png";
+import truck from "../../Static/110.png";
+import convertible from "../../Static/110.png";
+import compact from "../../Static/110.png";
+import electric from "../../Static/110.png";
+import hybrid from "../../Static/110.png";
+import passenger from "../../Static/110.png";
+import luxury from "../../Static/110.png";
+import other from "../../Static/110.png";
+import all from "../../Static/110.png";
+import car from "../../Static/110.png";
+export const data = [
+  {
+    id: 1,
+    name: "BMW",
+    category: "luxury",
+    image: car,
+    price: "$$$$",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    Rate: "4",
+  },
+  {
+    id: 1,
+    name: "PORSCHE",
+    category: "luxury",
+    image: car,
+    price: "$$$$",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    Rate: "4",
+  },
+  {
+    id: 1,
+    name: "Mercedes-Benz",
+    category: "luxury",
+    image: car,
+    price: "$$$$",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    Rate: "4",
+  },
+  {
+    id: 1,
+    name: "Honda",
+    category: "economy",
+    image: car,
+    price: "$$$$",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    Rate: "4",
+  },
+  {
+    id: 1,
+    name: "Ford",
+    category: "luxury",
+    image: car,
+    price: "$$$$",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    Rate: "4",
+  },
+];
+export const cityy = [
+  {
+    id: 0,
+    name: "Tehran",
+  },
+  {
+    id: 1,
+    name: "Khorasan-Razavi",
+  },
+  {
+    id: 2,
+    name: "Isfahan",
+  },
+  {
+    id: 3,
+    name: "Fars",
+  },
+  {
+    id: 4,
+    name: "Khuzestan",
+  },
+  {
+    id: 5,
+    name: "Azarbaijan East",
+  },
+  {
+    id: 6,
+    name: "Azarbaijan West",
+  },
+  {
+    id: 7,
+    name: "Mazandaran",
+  },
+  {
+    id: 8,
+    name: "Kerman",
+  },
+  {
+    id: 9,
+    name: "Albors",
+  },
+  {
+    id: 10,
+    name: "Gilan",
+  },
+  {
+    id: 11,
+    name: "Sistan-and-Baluchestan",
+  },
+  {
+    id: 12,
+    name: "Kermanshah",
+  },
+  {
+    id: 13,
+    name: "Hamedan",
+  },
+  {
+    id: 14,
+    name: "Lorestan",
+  },
+  {
+    id: 15,
+    name: "Golestan",
+  },
+  {
+    id: 16,
+    name: "Kurdistan",
+  },
+  {
+    id: 17,
+    name: "Hormozgan",
+  },
+  {
+    id: 18,
+    name: "Markazi",
+  },
+  {
+    id: 19,
+    name: "Ardabil",
+  },
+  {
+    id: 20,
+    name: "Qazvin",
+  },
+  {
+    id: 21,
+    name: "Qom",
+  },
+  {
+    id: 22,
+    name: "Bushehr",
+  },
+  {
+    id: 23,
+    name: "Yazd",
+  },
+  {
+    id: 24,
+    name: "Zanjan",
+  },
+  {
+    id: 25,
+    name: "Chahar-Mahaal-and-Bakhtairi",
+  },
+  {
+    id: 26,
+    name: "Khorasan-North",
+  },
+  {
+    id: 27,
+    name: "Khorasan-South",
+  },
+  {
+    id: 28,
+    name: "Kohgiluyeh-and-Boyer-Ahmad",
+  },
+  {
+    id: 29,
+    name: "Semnan",
+  },
+  {
+    id: 30,
+    name: "Ilam",
+  }, 
+];
+export const colors = [
+  {
+    id:0,
+    name:'Gray'
+  },
+  {
+    id:1,
+    name :'Black'
+  },
+  {
+    id:2,
+    name:'Navy blue'
+  }, {
+    id:3,
+    name:'White'
+  }, {
+    id:4,
+    name:'Brown'
+  }, {
+    id:5,
+    name:'Others'
+  },
+
+
+]
+export const gearboxx = [
+  {
+    id: 0,
+    name : 'manual'
+  },
+  {
+    id:1,
+    name :'automatic'
+  }
+]
+export const fuel = [
+  {
+    id: 0,
+    name: "Gasoline",
+  },
+  {
+    id: 1,
+    name: "Diesel",
+  },
+  {
+    id: 2,
+    name: "Liquefied Natural Gas",
+  },
+  {
+    id: 3,
+    name: "Electricity",
+  },
+  {
+    id: 4,
+    name: "Hybrid",
+  },
+  {
+    id: 5,
+    name: "Fuel Cell",
+  },
+  {
+    id: 6,
+    name: "Biodiesel and Ethanol",
+  },
+];
+export const coooler = [
+  {
+    id: 0,
+    name: "Yes",
+  },
+  {
+    id: 1,
+    name: "No",
+  },
+];
+
+export const categories = [
+  {
+    id: 0,
+    name: "All",
+    image: all,
+  },
+
+  {
+    id: 1,
+    name: "Economy",
+    image: economic,
+  },
+  {
+    id: 2,
+    name: "Luxury",
+    image: luxury,
+  },
+  {
+    id: 3,
+    name: "Compact",
+    image: compact,
+  },
+  {
+    id: 4,
+    name: "Offroad",
+    image: affroad,
+  },
+  {
+    id: 5,
+    name: "Hybrid",
+    image: hybrid,
+  },
+  {
+    id: 6,
+    name: "Electric",
+    image: electric,
+  },
+  {
+    id: 7,
+    name: "Truck",
+    image: truck,
+  },
+  {
+    id: 8,
+    name: "Convertible",
+    image: convertible,
+  },
+  {
+    id: 9,
+    name: "Passenger-van",
+    image: passenger,
+  },
+  {
+    id: 10,
+    name: "Mini-van",
+    image: minivan,
+  },
+  {
+    id: 11,
+    name: "Others",
+    image: other,
+  },
+];
