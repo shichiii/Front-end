4d084f7d780eec6caf0503992656435ecafaac5b
Hazhir2002 <Hazhiryousefi2002@gmail.com>
2023-11-13 11:52:09 +0330

    initial Create

--- shichi/src/page/CarInfoPage.js
+++ shichi/src/page/CarInfoPage.js
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import IconChat from "../Components/for_push/ChatPage/IconChat";
+import CarImageSlider from "../Components/for_push/CarInfo/CarImageSlider";
 function CarInfoPage() {
   const adv = useParams();
   const [refreshComment, setRefreshComment] = useState(false);
@@ -34,6 +35,7 @@ function CarInfoPage() {
             <CarDropOffLocation car={car} />
           </div>
           <CarInfo adv={adv.id} car={car} />
+          <CarImageSlider />
           {/* <CarOptionalExtras /> */}
           <BookCar adv={adv.id} />
           <AddComment adv={adv.id} setRefreshComment={setRefreshComment} />
