3ae57a66d43b0faf6511c8db342bcd2cdb72617c
fatemehnikpayan <fatemeh_nikpayan@comp.iust.ac.ir>
2023-12-28 21:34:25 +0330

    about us page and responsive edit profile

--- shichi/src/page/ProfilePage.js
+++ shichi/src/page/ProfilePage.js
@@ -2,30 +2,12 @@ import { useEffect, useState } from "react";
 import Navbar from "../Components/for_push/HomePage/NavBar";
 import Input from "../Components/for_push/Profile/Input";
 import ProfileImage from "../Components/for_push/Profile/ProfileImage";
-
-// import Modal from "react-modal";
-// import MyComponent from "../Components/addcar/popup"
-
-
-
 import Modal from "react-modal";
-
-//import MyComponent from "../Components/addcar/popup";
-
-// import MyComponent from "../Components/addcar/popup";
-
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-// import Modal from "react-modal";
-// import MyComponent from "../Components/addcar/popup"
-
-
-
-
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+import Footer from '../Components/for_push/HomePage/Footer'
 const baseURL = "http://87.107.105.201:8000/user/show/";
 
 function ProfilePage() {
@@ -46,6 +28,7 @@ function ProfilePage() {
       setPhoneNumber(response.data.phone_number);
     });
   }, [userId]);
+
   const [isDisabled, setIsDisabled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -57,7 +40,7 @@ function ProfilePage() {
       .put(`http://87.107.105.201:8000/user/update/${userId.id}/`, {
         first_name: firstN,
         last_name: lastN,
-        email: email,
+        phone_number: phoneNumber,
       })
       .then((response) => {
         console.log(response.data);
@@ -67,14 +50,15 @@ function ProfilePage() {
   }
 
   return (
-    <div className="bg-gradient-to-t from-pallate-Gunmetal via-pallate-Police_Blue to-pallate-Gunmetal h-screen">
+    <div>
+    <div className="bg-gradient-to-t from-pallate-Gunmetal via-pallate-Police_Blue to-pallate-Gunmetal min-h-screen flex flex-col ">
       <ToastContainer position="bottom-left" theme="dark" pauseOnHover />
       <Navbar />
-      <div className=" mx-auto w-[800px] flex flex-row rounded-2xl justify-center p-10 items-center mt-10 flex-wrap text-slate-300 font-bold font-mono bg-pallate-Dark_Sky_Blue bg-opacity-30 lg:bg-opacity-20">
+      <div className="mx-auto  xl:w-[70%] 2xl:w-[60%] flex flex-col rounded-2xl justify-center p-6 lg:p-10 items-center mt-10 flex-wrap text-slate-300 font-bold font-mono bg-pallate-Dark_Sky_Blue bg-opacity-30 lg:bg-opacity-20">
         <div className="w-full">
           <ProfileImage />
         </div>
-        <div className="w-full flex flex-row gap-10 items-center justify-center mt-10">
+        <div className="w-full flex flex-col lg:flex-row gap-6 lg:gap-10 items-center justify-center mt-6 lg:mt-10">
           <Input
             type="text"
             name="fName"
@@ -82,7 +66,7 @@ function ProfilePage() {
             value={firstN}
             setValue={setFirstN}
             disabled={isDisabled}
-            errorMessage={!firstN ? "first name cannot be empty" : null}
+            errorMessage={!firstN ? "First name cannot be empty" : null}
           />
           <Input
             type="text"
@@ -91,18 +75,10 @@ function ProfilePage() {
             value={lastN}
             setValue={setLastN}
             disabled={isDisabled}
-            errorMessage={!lastN ? "last name cannot be empty" : null}
+            errorMessage={!lastN ? "Last name cannot be empty" : null}
           />
         </div>
-        <div className="w-full flex flex-row gap-20 items-center justify-center mt-4">
-          {/* <Input
-            type="email"
-            name="email"
-            label="Email Address"
-            value={email}
-            setValue={setEmail}
-            disabled={isDisabled}
-          /> */}
+        <div className="w-full flex flex-col lg:flex-row gap-6 lg:gap-10 items-center justify-center mt-4 lg:mt-6">
           <Input
             type="text"
             name="phoneNumber"
@@ -113,42 +89,26 @@ function ProfilePage() {
             errorMessage={
               !phoneNumber || phoneNumber.length === 11
                 ? null
-                : "phoneNumber is not valid"
+                : "Phone number is not valid"
             }
           />
         </div>
         <button
           className={`${
             !isLoading
-              ? "bg-pallate-Dark_Sky_Blue hover:bg-transparent hover:text-pallate-Dark_Sky_Blue  text-white"
+              ? "bg-pallate-Dark_Sky_Blue hover:bg-transparent hover:text-pallate-Dark_Sky_Blue text-white"
               : "text-pallate-Dark_Sky_Blue bg-transparent"
-          } p-1  font-mono text-[20px] w-1/3  rounded-[400px] font-medium px-6 py-3 mt-12 mx-auto transition-all duration-300`}
+          } p-1 font-mono text-[18px] lg:text-[20px] w-full lg:w-[50%] rounded-[400px] font-medium px-6 py-3 mt-6 lg:mt-12 mx-auto lg:mx-0 transition-all duration-300`}
           onClick={() => editProfileHandler()}
           disabled={
-            isLoading ||
-            !firstN ||
-            !lastN ||
-            (!phoneNumber && phoneNumber?.length !== 11)
-              ? true
-              : false
+            isLoading || !firstN || !lastN || (!phoneNumber && phoneNumber?.length !== 11)
           }
         >
           {isLoading ? "Saving..." : "Save Changes"}
         </button>
-
-        {/* <MyComponent/> */}
-
-
-   
-
-
-        {/* <MyComponent/> */}
-
-
-
       </div>
-      {/* <MyComponent /> */}
-    </div>
+      
+    </div><Footer/></div>
   );
 }
 
