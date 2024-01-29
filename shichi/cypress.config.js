4e002c301d75d44810630d872dc6ac3878d1427c
fatemehnikpayan <fatemeh_nikpayan@comp.iust.ac.ir>
2024-01-29 17:49:07 +0330

    test: fix test and coverge

--- shichi/cypress.config.js
+++ shichi/cypress.config.js
@@ -8,15 +8,25 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-      // on(
-      //   'file:preprocessor',
-      //   require('@cypress/code-coverage/use-browserify-istanbul')
-      // )
-      // include any other plugin code...
 
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
-      return config
+      // on(
+        //   'file:preprocessor',
+        //   require('@cypress/code-coverage/use-browserify-istanbul')
+        // )
+        // include any other plugin code...
+        
+        // It's IMPORTANT to return the config object
+        // with any changed environment variables
+        return config
+      },
+      specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
     },
-  },
+    component: {
+      setupNodeEvents(on, config) {},
+      devServer: {
+        framework: 'create-react-app',
+        bundler: 'webpack'
+      },
+      specPattern: ['**/**/*.comp-spec.{js,ts,jsx,tsx}', '**/**/*.cy.{js,ts,jsx,tsx}']
+    }
 })
--- shichi/cypress/e2e/show_Compact_category_with_homepage.cy.js
+++ shichi/cypress/e2e/show_Compact_category_with_homepage.cy.js
@@ -8,13 +8,7 @@ describe('show_Compact_category_with_homepage', () => {
       cy.get("#email").type("atranikpayan@gmail.com");
       cy.get("#password").type("atra1234", { log: false });
       cy.contains('Login').click()
-<<<<<<< HEAD
-      cy.wait(10000);
-
-      // cy.wait()
-=======
       cy.wait(5000)
->>>>>>> 98be19059baf3e8590b28be673051e98a9dfc8fa
 
 
       cy.contains('Show Compact').click()
